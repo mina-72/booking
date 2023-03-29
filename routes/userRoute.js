07c4ce3f2d727150618841a145c1f427624367c3
mina_72 <mina.mahdizadeh91@gmail.com>
2023-03-30 02:09:22 +0330

    removed test model

--- routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const userController = require("../controllers/userController");
-
-const router = require("express").Router();
-
-router.post("/adduser", userController.addUser);
-router.get("/getuser", userController.getAllUsers);
-
-module.exports = router;
