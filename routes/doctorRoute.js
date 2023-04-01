e97cf744d9074c3d7636767553338d78b4e9b652
mina_72 <mina.mahdizadeh91@gmail.com>
2023-04-01 23:16:03 +0330

    move controllers code to other controllers

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -7,14 +7,6 @@ router.post("/adddoctor", doctorController.createDoctor);
 
 //get all doctors
 const { getAllDoctors } = require("../controllers/doctorController");
-router.get("/getalldoctors", async (req, res, next) => {
-  try {
-    const doctors = await getAllDoctors();
-    res.status(200).json({ doctors: doctors });
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
+router.get("/getalldoctors", doctorController.getAllDoctors);
 
 module.exports = router;
