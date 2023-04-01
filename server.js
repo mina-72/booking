6cbc374225225a0769cecba9038ad4485fd65edc
mina_72 <mina.mahdizadeh91@gmail.com>
2023-04-01 13:19:23 +0330

    clean up

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-var corOptions = {
-  origin: "https://localhost:3000",
-};
-
-//middlewares
-app.use(cors(corOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//routes
 const doctorRoute = require("./routes/doctorRoute");
 const specialitiesRoute = require("./routes/specialitiesRoute");
 const patientRoute = require("./routes/patientRoute");
@@ -22,11 +14,6 @@ app.use("/api/doctor", doctorRoute);
 app.use("/api/specialities", specialitiesRoute);
 app.use("/api/patient", patientRoute);
 app.use("/api/appointment", appointmentRoute);
-
-//TESTING routes
-app.get("/", (req, res) => {
-  res.send({ message: "heelo" });
-});
 
 const PORT = process.env.PORT || 8000;
 
