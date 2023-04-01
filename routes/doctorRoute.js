1ac4659b308042f0cba00731feaece03f191ac0d
mina_72 <mina.mahdizadeh91@gmail.com>
2023-04-01 13:16:43 +0330

    add getAllDoctors to doctor route

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -2,6 +2,19 @@ const doctorController = require("../controllers/doctorController");
 
 const router = require("express").Router();
 
+//create new doctor
 router.post("/adddoctor", doctorController.createDoctor);
-router.get("/getdoctor", doctorController.getDoctor);
+
+//get all doctors
+const { getAllDoctors } = require("../controllers/doctorController");
+router.get("/getalldoctors", async (req, res, next) => {
+  try {
+    const doctors = await getAllDoctors();
+    res.status(200).json({ doctors: doctors });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
 module.exports = router;
